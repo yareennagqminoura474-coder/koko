5bf2fda78f736803b5e8d2684379e0849ae02323
yareennagqminoura474-coder <yareennagqminoura474@gmail.com>
2026-05-26 16:07:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const SW_VERSION = '20260526-echoes-group-fix-1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil((async () => {
+        const cacheKeys = await caches.keys();
+        await Promise.all(cacheKeys.map(cacheKey => caches.delete(cacheKey)));
+        await self.clients.claim();
+    })());
+});
+
+self.addEventListener('message', event => {
+    if (event.data !== 'CLEAR_CACHES') return;
+    event.waitUntil((async () => {
+        const cacheKeys = await caches.keys();
+        await Promise.all(cacheKeys.map(cacheKey => caches.delete(cacheKey)));
+    })());
+});
+
+self.addEventListener('fetch', () => {
+    // Keep network responses uncached so updated index.html is always used.
+});
