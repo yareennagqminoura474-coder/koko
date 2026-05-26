8cabfc12944d35fbb6a54740871b0bce5cf85396
yareennagqminoura474-coder <yareennagqminoura474@gmail.com>
2026-05-26 16:38:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '20260526-echoes-group-fix-1';
+const SW_VERSION = '20260526-echoes-group-fix-2';
 
 self.addEventListener('install', event => {
     event.waitUntil(self.skipWaiting());
